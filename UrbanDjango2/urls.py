0227771813532797e9d8fa1361a72e5e53a33213
YaEd71 <yaeduard71@gmail.com>
2024-10-10 22:34:49 +0300

    19_3

--- UrbanDjango2/urls.py
+++ UrbanDjango2/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from task1 import views as task1_views
 
 urlpatterns = [
+    path('', task1_views.sign_up_by_html, name='sign_up_by_html'),
+    path('django_sign_up', task1_views.sign_up_by_django, name='sign_up_by_django'),
     path('admin/', admin.site.urls),
+    path('platform/', task1_views.platform, name='platform'),
+    path('platform/games/', task1_views.games, name='games'),
+    path('platform/cart/', task1_views.cart, name='cart'),
 ]
--- task1/views.py
+++ task1/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from .forms import UserRegister
+from .models import Buyer, Game
+from django.db.models import Q
 
-# Create your views here.
+def sign_up_by_django(request):
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            if password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif Buyer.objects.filter(name=username).exists():
+                info['error'] = 'Пользователь уже существует'
+            else:
+                Buyer.objects.create(name=username, balance=0, age=age)
+                return render(request, 'fifth_task/registration_page.html', {'message': f'Приветствуем, {username}!'})
+    else:
+        form = UserRegister()
+
+    info['form'] = form
+    return render(request, 'fifth_task/registration_page.html', info)
+
+def sign_up_by_html(request):
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = int(request.POST.get('age', 0))
+
+        if password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        elif age < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        elif Buyer.objects.filter(name=username).exists():
+            info['error'] = 'Пользователь уже существует'
+        else:
+            Buyer.objects.create(name=username, balance=0, age=age)
+            return render(request, 'fifth_task/registration_page.html', {'message': f'Приветствуем, {username}!'})
+
+    return render(request, 'fifth_task/registration_page.html', info)
+
+def platform(request):
+    return render(request, 'fourth_task/platform.html')
+
+def games(request):
+    games_list = Game.objects.all()
+    return render(request, 'fourth_task/games.html', {'games': games_list})
+
+def cart(request):
+    return render(request, 'fourth_task/cart.html')
